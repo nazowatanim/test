39a095dd665063c68f64dade525a4d73c2fc4c53
“nazowatanim“ <nazowatanim15@gmail.com>
2019-01-03 11:52:35 +0100

    commited

--- stats.py
+++ stats.py
@@ -4,6 +4,8 @@ import json
 from flask import Flask, request, Response
 import json #for dumping JSON format
 import requests
+import subprocess
+import random
 
 app = Flask(__name__)
 
@@ -12,11 +14,26 @@ app = Flask(__name__)
 def test():
     return 'server has been created'
 
+@app.route("/cpuusage", methods=['GET'])
+def cpu_usage():
+    try:
+
+        json_key = {}
+        cpu_usage = psutil.cpu_percent(interval=2)
+        json_key['cpu_usage'] = float(cpu_usage)
+        y=json.dumps(json_key)
+        print (y)
+        return  (y)
+    except requests.exceptions.HTTPError:
+        pass
+
 @app.route("/cpu", methods=['GET'])
 def cpu():
     try:
-        x=psutil.cpu_times()
-        return(str(x))
+        x= psutil.cpu_times()
+        print(x)
+        #return (x)
+        return  '{'+'\n'+'"'+'CPU usage time'+'"'+':'+(str(x))+'\n'+'}'
     except requests.exceptions.HTTPError:
         pass
 
@@ -26,36 +43,44 @@ def memory():
         x=psutil.virtual_memory()
         y=psutil.swap_memory()
         z=psutil.disk_usage('/')
-        return('virtual memory: '+str(x)+'\n'+'swap memory: '+str(y)+'\n'+'disk usage: '+str(z))
+        return '{'+'\n'+'"'+'memory'+'"'+':'+(str(x)+'\n'+'"'+'swap memory'+'"'+':'+str(y)+'\n'+'"'+'disk usage'+'"'+':'+str(z))+'\n'+'}'
     except requests.exceptions.HTTPError:
         pass
 
 @app.route("/network", methods=['GET'])
 def network():
     try:
-        #x=psutil.net_io_counters(pernic=True)
-        y=psutil.net_if_addrs()
-        z=psutil.net_if_stats()
-        #a=psutil.users()
-        return('network address: '+str(y)+'\n'+'Interface stats: '+str(z))#+'\n'+'users: '+str(a))
+        json_key={}
+        output = subprocess.Popen(["netengine-utils ifconfig"], shell=True, stdout=subprocess.PIPE).communicate()[0]
+        print(output)
+        return (output)
     except requests.exceptions.HTTPError:
         pass
+
+@app.route("/net", methods=['GET'])
+def network2():
+    try:
+        y = psutil.net_io_counters(pernic=True)
+        print(y)
+    except requests.exceptions.HTTPError:
+        pass
+
 
 @app.route("/sensor", methods=['GET'])
 def sensor():
     try:
-        x=psutil.sensors_temperatures()
-        y=psutil.sensors_fans()
-        z=psutil.sensors_battery()
-        return('sensor temperatures : '+str(x)+'\n'+'sensor fans: '+str(y)+'\n'+'sensors battery: '+str(z))
+        x=psutil.sensors_battery()
+        print (x)
+        return '{'+'\n'+'"'+'memory'+'"'+'sensors battery: '+str(x)+'\n'+'}'
     except requests.exceptions.HTTPError:
         pass
+
 @app.route("/process", methods=['GET'])
 def Process_Management():
     try:
         x=psutil.pids()
         #z=psutil.sensors_battery()
-        return('system pids: '+str(x))
+        return('pids:  '+str(x))
     except requests.exceptions.HTTPError:
         pass
 
@@ -79,5 +104,7 @@ def PID_Info():
 
 
 
+
 if __name__ == "__main__":
+
     app.run(host='0.0.0.0', port=5000)
